8d115407ed38cbd389e16a4fb25eba5b9a95cb2e
Silen Wang <qiumin14@163.com>
2019-10-07 14:19:55 +0800

    First commit Repitope dockerfile

---
+++ README.md
@@ -0,0 +1,5 @@
+# Dockerfiles
+
+Self-made dockerfiles to build some bioinformatic tools that are unavailable from conda and can't be easily installed because they are still in the early-stage.
+
+- Repitope: A tool to predict pMHC-
---
+++ Repitope/install.r
@@ -0,0 +1,7 @@
+options("repos" = c(CRAN="https://mirrors.ustc.edu.cn/CRAN/"))
+options(BioC_mirror="http://mirrors.ustc.edu.cn/bioc/")
+install.packages(c("BiocManager", "devtools", "lazyeval", "labeling"))
+install.packages(c('ggplot2','ggthemes','officer','rvg','scales','tidyverse','reshape2','tibble','gdtools','labeling','munsell','RColorBrewer','broom','dplyr','dbplyr','forcats','haven','hms','lubridate','modelr','readr','readxl','reprex','rvest','tidyr','plyr','colorspace','fansi','pillar','pkgconfig','utf8','vctrs','ellipsis','zeallot','systemfonts','generics','tidyselect','BH','plogr','DBI','cellranger','progress','rematch','rmarkdown','knitr','tinytex','xfun','highr','markdown','selectr'))
+BiocManager::install(c('Biostrings', 'msa', 'S4Vectors', 'car', 'caret', 'cvAUC', 'DescTools', 'doParallel', 'doSNOW', 'extraTrees', 'foreach', 'fst', 'ggpubr', 'ggsci', 'matrixStats', 'mlr', 'Peptides', 'seqinr', 'stringdist', 'survminer', 'VennDiagram'))
+suppressWarnings(devtools::install_github("masato-ogishi/plotUtility"))
+suppressWarnings(devtools::install_github("masato-ogishi/Repitope"))
